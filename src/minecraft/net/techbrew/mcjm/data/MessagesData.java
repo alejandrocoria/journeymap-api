702e74371161aff66c8ba2968d1b844527d107e0
Mark Woodman <mark.a.woodman@gmail.com>
2013-01-27 15:44:07 -0700

    MessagesServlet replaced with MessagesData

---
+++ src/minecraft/net/techbrew/mcjm/data/MessagesData.java
@@ -0,0 +1,74 @@
+package net.techbrew.mcjm.data;
+
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.src.WorldInfo;
+import net.techbrew.mcjm.Constants;
+
+/**
+ * Provides game-related properties in a Map.
+ * 
+ * @author mwoodman
+ *
+ */
+public class MessagesData implements IDataProvider {
+	
+	private static final String KEY_PREFIX = "WebMap."; //$NON-NLS-1$
+	
+	private static long TTL = TimeUnit.DAYS.toMillis(1);
+	
+	public static enum Key {
+		messages;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public MessagesData() {
+	}
+	
+	/**
+	 * Provides all possible keys.
+	 */
+	public Enum[] getKeys() {
+		return Key.values();
+	}
+		
+	/**
+	 * Return map of web-UI messages.
+	 */
+	public Map getMap() {		
+		
+		LinkedHashMap props = new LinkedHashMap();
+		
+		props.put("locale", Constants.getLocale());
+		ResourceBundle bundle = Constants.getResourceBundle();
+
+		Set<String> allKeys = Constants.getBundleKeys();
+		for(String key : allKeys) {
+			if(key.startsWith(KEY_PREFIX)) {
+				String name = key.split(KEY_PREFIX)[1];
+				String value = Constants.getString(key, bundle);
+				props.put(name, value);
+			}
+		}
+
+		return props;
+	}
+	
+	/**
+	 * Return length of time in millis data should be kept.
+	 */
+	public long getTTL() {
+		return TTL;
+	}
+	
+}
