f9e0a98067493c31ac11f40acaad2e0250d21088
Mark Woodman <mark.a.woodman@gmail.com>
2013-01-27 15:45:13 -0700

    Adding JSONP callback support

--- src/minecraft/net/techbrew/mcjm/server/DataService.java
+++ src/minecraft/net/techbrew/mcjm/server/DataService.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.channels.Channel;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
@@ -41,10 +42,12 @@ import net.techbrew.mcjm.data.AnimalsData;
 import net.techbrew.mcjm.data.DataCache;
 import net.techbrew.mcjm.data.GameData;
 import net.techbrew.mcjm.data.IDataProvider;
+import net.techbrew.mcjm.data.MessagesData;
 import net.techbrew.mcjm.data.MobsData;
 import net.techbrew.mcjm.data.PlayerData;
 import net.techbrew.mcjm.data.PlayersData;
 import net.techbrew.mcjm.data.TimeData;
+import net.techbrew.mcjm.data.VillagersData;
 import net.techbrew.mcjm.data.WorldData;
 import net.techbrew.mcjm.io.ChunkFileHandler;
 import net.techbrew.mcjm.io.FileHandler;
@@ -72,15 +75,18 @@ public class DataService extends BaseService {
 	
 	public static final HashMap<String,Class<? extends IDataProvider>> providerMap;
 	static {
-		providerMap = new HashMap<String,Class<? extends IDataProvider>>(7);
-		providerMap.put("/data/game", GameData.class);  //$NON-NLS-1$
-		providerMap.put("/data/player", PlayerData.class);  //$NON-NLS-1$
-		providerMap.put("/data/time", TimeData.class);  //$NON-NLS-1$
-		providerMap.put("/data/world", WorldData.class);  //$NON-NLS-1$
+		providerMap = new HashMap<String,Class<? extends IDataProvider>>(8);
 		providerMap.put("/data/animals", AnimalsData.class);  //$NON-NLS-1$
 		providerMap.put("/data/mobs", MobsData.class);  //$NON-NLS-1$
+		providerMap.put("/data/game", GameData.class);  //$NON-NLS-1$
+		providerMap.put("/data/messages", MessagesData.class);  //$NON-NLS-1$
+		providerMap.put("/data/player", PlayerData.class);  //$NON-NLS-1$
 		providerMap.put("/data/players", PlayersData.class);  //$NON-NLS-1$
+		providerMap.put("/data/time", TimeData.class);  //$NON-NLS-1$
+		providerMap.put("/data/world", WorldData.class);  //$NON-NLS-1$
+		providerMap.put("/data/villagers", VillagersData.class);  //$NON-NLS-1$
 		
+		// Compose path string used by RupyService
 		StringBuffer sb = new StringBuffer();
 		for(String key : providerMap.keySet()) {
 			sb.append(key).append(":");
@@ -104,45 +110,50 @@ public class DataService extends BaseService {
 	
 	@Override
 	public void filter(Event event) throws Event, Exception {
-		
-		// Ensure JourneyMap and World is loaded
-		if(!JourneyMap.isRunning()) {
-			throwEventException(503, Constants.getMessageJMERR02(), event, false);
-		} else if(Minecraft.getMinecraft().theWorld==null) {
-			throwEventException(503, Constants.getMessageJMERR09(), event, false);
-		}
 
 		// Parse query for parameters
 		Query query = event.query();
 		query.parse();
 		String path = query.path();
 		
-		// Buffer the output
-		StringBuffer jsonData = new StringBuffer();
-		
-		// Check for callback
-		if(query.containsKey(CALLBACK_PARAM)) {
-			jsonData.append(query.get(CALLBACK_PARAM).toString()); 
-		} else {
-			jsonData.append(CALLBACK_PARAM);
-		}		
-		jsonData.append("("); //$NON-NLS-1$	
+		// If not a request for L10N, check world/minecraft status
+		if(!path.equals("/data/messages")) {		
+			// Ensure JourneyMap and World is loaded
+			if(!JourneyMap.isRunning()) {
+				throwEventException(503, Constants.getMessageJMERR02(), event, false);
+			} else if(Minecraft.getMinecraft().theWorld==null) {
+				throwEventException(503, Constants.getMessageJMERR09(), event, false);
+			}
+		}
 		
 		// Get cached data provider keyed by the path
-		Class<? extends IDataProvider> dpClass = providerMap.get(path);
+		Class<? extends IDataProvider> dpClass = providerMap.get(path);		
+		
+		// Build the response string
+		StringBuffer jsonData = new StringBuffer();
 				
-		// Get data expiration time and append data from cache
-		long expires = DataCache.instance().appendJson(dpClass, jsonData);	
+		// Check for callback to determine Json or JsonP
+		boolean useJsonP = query.containsKey(CALLBACK_PARAM);
+		if(useJsonP) {
+			jsonData.append(URLEncoder.encode(query.get(CALLBACK_PARAM).toString(), UTF8.name()));
+			jsonData.append("("); //$NON-NLS-1$	
+		} else {
+			jsonData.append("data="); //$NON-NLS-1$	
+		}	
 		
-		jsonData.append(")"); //$NON-NLS-1$	
+		// Append the data
+		DataCache.instance().appendJson(dpClass, jsonData);	
 		
-		// Optimize headers
+		// Finish function call for JsonP if needed
+		if(useJsonP) {
+			jsonData.append(")"); //$NON-NLS-1$
+		}
+		
+		// Optimize headers for JSONP
 		ResponseHeader.on(event).noCache().contentType(ContentType.jsonp);
 				
 		// Gzip response
 		gzipResponse(event, jsonData.toString());
 	}
-	
-	
 	
 }
