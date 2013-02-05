eea3b1cfecbde82c71f0a337a41ccc77fe0618f0
Mark Woodman <mark.a.woodman@gmail.com>
2013-02-05 01:39:20 -0700

    Improving rendering algorithm

--- src/minecraft/net/techbrew/mcjm/render/ChunkTerrainRenderer.java
+++ src/minecraft/net/techbrew/mcjm/render/ChunkTerrainRenderer.java
@@ -55,7 +55,7 @@ public class ChunkTerrainRenderer implements IChunkRenderer {
 			int arrSize = 256;
 	
 			float[] lightDir = new float[]{-100,1,-100};
-			intersectMapOptimized(chunkStub, 16, chunkImage, neighbors); 
+			shadeChunk(chunkStub, chunkImage, neighbors); 
 			
 		} catch(Throwable t) {
 			t.printStackTrace();
@@ -68,65 +68,79 @@ public class ChunkTerrainRenderer implements IChunkRenderer {
 	/**
 	 * http://content.gpwiki.org/index.php/Faster_Ray_Traced_Terrain_Shadow_Maps#Optimized_Source_Code
 	 */
-	void intersectMapOptimized(ChunkStub chunkStub, int size, BufferedImage chunkImage, Map<Integer, ChunkStub> neighbors)
+	void shadeChunk(ChunkStub chunkStub, BufferedImage chunkImage, Map<Integer, ChunkStub> neighbors)
 	{
-		// create flag buffer to indicate where we've been
-		float[] flagMap = new float[size*size];
 		
-		float h, hN, hNW, hW;
-		float s=1f, sN, sNW, sW;
+		float h, hN, hW;
+		float s=1f;
 		int shaded;
 		float slope;
 		float intensity = 1f;
+		boolean paintNight = chunkImage.getWidth()>16;
 		
 		if(chunkStub.slopes==null) {
-			chunkStub.slopes = new float[size][size];
-			for(int x=0; x<size; x++)
+			chunkStub.slopes = new float[16][16];
+			float minNorm = chunkStub.worldHeight;
+			float maxNorm = 0;
+			for(int y=0; y<16; y++)
 			{
-				Arrays.fill(chunkStub.slopes[x], 1f);
+				for(int x=0; x<16; x++)
+				{				
+					h = chunkStub.getSafeHeightValue(x, y);
+					hN = (y==0)  ? getBlockHeight(x, y, 0, -1, chunkStub, neighbors, h) : chunkStub.getSafeHeightValue(x, y-1);							
+					hW = (x==0)  ? getBlockHeight(x, y, -1, 0, chunkStub, neighbors, h) : chunkStub.getSafeHeightValue(x-1, y);
+					slope = ((h/hN)+(h/hW))/2f;
+					chunkStub.slopes[x][y] = slope;						
+				}
 			}
 		}
 		
-		for(int y=0; y<size; y++)
+		float sN, sNW, sW, sAvg;		
+		for(int z=0; z<16; z++)
 		{
-			for(int x=0; x<size; x++)
+			for(int x=0; x<16; x++)
 			{				
-				h = chunkStub.getSafeHeightValue(x, y);
-				hN = (y==0) ? getBlockHeight(x, y, 0, -1, chunkStub, neighbors, h) : chunkStub.getSafeHeightValue(x, y-1);
-				hNW = (x==0 || y==0) ? getBlockHeight(x, y, -1, -1, chunkStub, neighbors, h) : chunkStub.getSafeHeightValue(x-1, y-1);
-				hW = (x==0) ? getBlockHeight(x, y, -1, 0, chunkStub, neighbors, h) : chunkStub.getSafeHeightValue(x-1, y);
-				float avgH = (hN + hNW + hW) / 3.0f;
-
-				s = (h/avgH);	
-								
-				if(s!=1f) {
-					
-					sN = (y==0) ? getBlockSlope(x, y, 0, -1, chunkStub, neighbors, s) : chunkStub.slopes[x][y-1];
-					sNW = (x==0 || y==0) ? getBlockSlope(x, y, -1, -1, chunkStub, neighbors, s) : chunkStub.slopes[x-1][y-1];
-					sW = (x==0) ? getBlockSlope(x, y, -1, 0, chunkStub, neighbors, s) : chunkStub.slopes[x-1][y];
-					float avgS = (sN + sNW + sW) / 3.0f;
-					
-					if(s<1) {
-						
-						s = (float) (s + Math.log(s*.8f));
-						s = Math.min(s, avgS*.92f);
-						s = Math.max(s, .3f);						
-						
-					} else if(s>1) {
-						
-						s = (float) (s + Math.log(s));
-						s = Math.min(s, Math.max(sN, 1.0f));
-						s = Math.min(s, Math.max(sNW, 1.0f));
-						s = Math.min(s, Math.max(sW, 1.0f));
-						s = s*1.1f;
-					}				
-					
-				}
+				slope = chunkStub.slopes[x][z];
 				
-				chunkStub.slopes[x][y] = s;
+				sN = getBlockSlope(x, z, 0, -1, chunkStub, neighbors, slope);
+				sNW = getBlockSlope(x, z, -1, -1, chunkStub, neighbors, slope);
+				sW = getBlockSlope(x, z, -1, 0, chunkStub, neighbors, slope);
+				sAvg = (sN+sNW+sW)/3f;
 				
-				shaded = darken(chunkImage.getRGB(x, y), s);
-				chunkImage.setRGB(x,y,shaded);
+				if(slope<1) {
+					
+					if(slope<=sAvg) {
+						slope = slope*.6f;
+					} else if(slope>sAvg) {
+						slope = (slope+sAvg)/2f;
+					}
+					s = Math.max(slope * .8f, .1f);
+					
+					shaded = darken(chunkImage.getRGB(x, z), s);
+					chunkImage.setRGB(x,z,shaded);
+					
+					if(paintNight) {
+						shaded = darken(chunkImage.getRGB(x+16, z), s);
+						chunkImage.setRGB(x+16,z,shaded);
+					}
+					
+				} else if(slope>1) {
+					if(sAvg>1) {
+						if(slope>=sAvg) {
+							slope = slope*1.15f;
+						}
+					}
+					s = (float) slope * 1.15f;
+					s = Math.min(s, 1.4f);
+					
+					shaded = darken(chunkImage.getRGB(x, z), s);
+					chunkImage.setRGB(x,z,shaded);
+					
+					if(paintNight) {
+						shaded = darken(chunkImage.getRGB(x+16, z), s);
+						chunkImage.setRGB(x+16,z,shaded);
+					}
+				} 
 				 				
 			}
 		}
@@ -138,9 +152,11 @@ public class ChunkTerrainRenderer implements IChunkRenderer {
 		if(factor<0) {
 			throw new IllegalArgumentException("factor can't be negative");
 		}
-		int r = Math.min(255, (int) (((original >> 16) & 0xFF) * factor));
-		int g = Math.min(255, (int) (((original >> 8) & 0xFF) * factor));
-		int b = Math.min(255, (int) (((original >> 0) & 0xFF) * factor));
+		float bluer = (factor>=1) ? 1f : .9f;
+		
+		int r = Math.max(0, Math.min(255, (int) (((original >> 16) & 0xFF) * (factor*bluer))));
+		int g = Math.max(0, Math.min(255, (int) (((original >> 8) & 0xFF) * (factor*bluer))));
+		int b = Math.max(0, Math.min(255, (int) (((original >> 0) & 0xFF) * (factor))));
 
 		try {
 			return new Color(r,g,b).getRGB();
