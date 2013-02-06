a2fb486da34e0ef9562e497992ffce535b7904c4
Mark Woodman <mark.a.woodman@gmail.com>
2013-02-06 01:01:32 -0700

    Added timeout prop

--- src/minecraft/net/techbrew/mcjm/server/JMServer.java
+++ src/minecraft/net/techbrew/mcjm/server/JMServer.java
@@ -23,6 +23,7 @@ public class JMServer {
 		port = PropertyManager.getInstance().getInteger(PropertyManager.WEBSERVER_PORT_PROP);
 		props.put("port", Integer.toString(port)); //$NON-NLS-1$
 		props.put("delay", Integer.toString(10000)); //$NON-NLS-1$
+		props.put("timeout", Integer.toString(10000)); //$NON-NLS-1$
 		
 		// Instantiate daemon
 		rupy = new Daemon(props);
