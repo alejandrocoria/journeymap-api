57c0f4907c0afe741e23cc72181b3af9bd548ccd
Mark Woodman <mark.a.woodman@gmail.com>
2013-02-14 01:48:25 -0700

    Moved base functionality here

--- src/minecraft/net/techbrew/mcjm/render/BaseRenderer.java
+++ src/minecraft/net/techbrew/mcjm/render/BaseRenderer.java
@@ -17,7 +17,7 @@ import net.techbrew.mcjm.PropertyManager;
 public abstract class BaseRenderer {
 
 	final MapBlocks mapBlocks;	
-	final boolean caveLighting = PropertyManager.getInstance().getBoolean(PropertyManager.CAVE_LIGHTING_PROP);
+	boolean caveLighting = PropertyManager.getInstance().getBoolean(PropertyManager.CAVE_LIGHTING_PROP);
 	final boolean fineLogging = JourneyMap.getLogger().isLoggable(Level.FINE);
 	
 	boolean debug = false;
@@ -26,6 +26,7 @@ public abstract class BaseRenderer {
 	public BaseRenderer(MapBlocks mapBlocks) {
 		super();
 		this.mapBlocks = mapBlocks;
+		caveLighting = true;
 	}
 
 	/**
@@ -90,9 +91,23 @@ public abstract class BaseRenderer {
 	public void paintClearBlock(final int x, final int vSlice, final int z,
 			final Graphics2D g2D) {
 		g2D.setComposite(MapBlocks.OPAQUE);
-		g2D.setPaint(MapBlocks.COLOR_TRANSPARENT);
-		g2D.fillRect(x, z, 1, 1);
+		g2D.setBackground(MapBlocks.COLOR_TRANSPARENT);
 		g2D.clearRect(x, z, 1, 1);
+	}
+	
+	/**
+	 * Paint the block.
+	 * 
+	 * @param x
+	 * @param vSlice
+	 * @param z
+	 * @param color
+	 */
+	public void paintBlock(final int x, final int vSlice, final int z, final Color color,
+			final Graphics2D g2D) {
+		g2D.setComposite(MapBlocks.OPAQUE);
+		g2D.setColor(color);
+		g2D.fillRect(x, z, 1, 1);
 	}
 	
 	/**
@@ -188,6 +203,53 @@ public abstract class BaseRenderer {
 			} else {
 				return chunk.slopes[newX][newZ];
 			}
+		} else {
+			return defaultVal;
+		}
+	}
+	
+	/**
+	 * Get the block at the coordinates + offsets.  Uses ChunkStub.slopes.
+	 * @param x
+	 * @param z
+	 * @param offsetX
+	 * @param offsetz
+	 * @param currentChunk
+	 * @param neighbors
+	 * @param defaultVal
+	 * @return
+	 */
+	public BlockInfo getBlock(int x, int y, int z, int offsetX, int offsetz, ChunkStub currentChunk, Map<Integer, ChunkStub> neighbors, BlockInfo defaultVal) {
+		int newX = x+offsetX;
+		int newZ = z+offsetz;
+		
+		int chunkX = currentChunk.xPosition;
+		int chunkZ = currentChunk.zPosition;
+		boolean search = false;
+		
+		if(newX==-1) {
+			chunkX--;
+			newX = 15;
+			search = true;
+		} else if(newX==16) {
+			chunkX++;
+			newX = 0;
+			search = true;
+		}
+		if(newZ==-1) {
+			chunkZ--;
+			newZ = 15;
+			search = true;
+		} else if(newZ==16) {
+			chunkZ++;
+			newZ = 0;
+			search = true;
+		}
+		
+		ChunkStub chunk = getChunk(x, z, offsetX, offsetz, currentChunk, neighbors);
+		
+		if(chunk!=null) {
+			return mapBlocks.getBlockInfo(chunk, newX, y, newZ);
 		} else {
 			return defaultVal;
 		}
