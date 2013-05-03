d0e33ff9d12f89cf3b18d6e7641404032948cb07
mwoodman <mark.a.woodman@gmail.com>
2013-05-03 00:05:09 -0600

    Offsets now correctly used

--- src/minecraft/net/techbrew/mcjm/io/PngjHelper.java
+++ src/minecraft/net/techbrew/mcjm/io/PngjHelper.java
@@ -22,12 +22,12 @@ public class PngjHelper {
 
 	/**
 	 * @param tiles  Filenames of PNG files to tile
-	 * @param dest   Destination PNG filename
+	 * @param destFile   Destination PNG filename
 	 * @param tileColumns How many tiles per row?
 	 * 
 	 * Original: https://code.google.com/p/pngj/wiki/Snippets
 	 */
-	public static void mergeFiles(File tiles[], File dest, int tileColumns, int xOffset, int xWidth) {
+	public static void mergeFiles(File tiles[], File destFile, int tileColumns, int xOffset, int xWidth) {
 	    int ntiles = tiles.length;
 	    int tileRows = (ntiles + tileColumns - 1) / tileColumns; // integer ceil
 	    
@@ -37,7 +37,7 @@ public class PngjHelper {
 	    imi1 = pngr.imgInfo;
 	    PngReader[] readers = new PngReader[tileColumns];
 	    imi2 = new ImageInfo(xWidth * tileColumns, imi1.rows * tileRows, imi1.bitDepth, imi1.alpha, imi1.greyscale, imi1.indexed);
-	    PngWriter pngw = FileHelper.createPngWriter(dest, imi2, true);
+	    PngWriter pngw = FileHelper.createPngWriter(destFile, imi2, true);
 	    
 	    // copy palette and transparency if necessary (more chunks?)
 	    pngw.copyChunksFirst(pngr, ChunkCopyBehaviour.COPY_PALETTE | ChunkCopyBehaviour.COPY_TRANSPARENCY);
@@ -49,25 +49,37 @@ public class PngjHelper {
 	    for( int ty = 0; ty < tileRows; ty++ ) {
 	        int nTilesXcur = ty < tileRows - 1 ? tileColumns : ntiles - (tileRows - 1) * tileColumns;
 	        Arrays.fill(line2.scanline, 0);
-	        for( int tx = 0; tx < nTilesXcur; tx++ ) { // open serveral readers
+	        
+	        for( int tx = 0; tx < nTilesXcur; tx++ ) { // open several readers
 	            readers[tx] = FileHelper.createPngReader(tiles[tx + ty * tileColumns]);
 	            readers[tx].setChunkLoadBehaviour(ChunkLoadBehaviour.LOAD_CHUNK_NEVER);
 	            readers[tx].setUnpackedMode(false);
 	            if(!readers[tx].imgInfo.equals(imi1)) 
 	                throw new RuntimeException("different tile ? "  + readers[tx].imgInfo);
 	        }
-	        for(int row1 = 0; row1 < imi1.rows; row1++, row2++ ) {
+	        
+	        rowcopy: for(int row1 = 0; row1 < imi1.rows; row1++, row2++ ) {
 	            for( int tx = 0; tx < nTilesXcur; tx++ ) {
 	                ImageLine line1 = readers[tx].readRowInt(row1); // read line
+	                
 	                int len = xWidth * imi1.bytesPixel;
-	                try {
-	                	System.arraycopy(line1.scanline, xOffset*imi1.bitDepth, line2.scanline, len * tx, len);
-	                } catch(Exception e) {
-	                	JourneyMap.getLogger().severe("Bad data: " + readers[tx]);
+	                
+	                int[] src = line1.scanline;
+                	int srcPos = xOffset * imi1.bytesPixel;	                	
+                	int[] dest = line2.scanline;
+                	int destPos = (len*tx);
+                	int length = len;
+	                try {	                	
+	                	System.arraycopy(src, srcPos, dest, destPos, length);
+	                } catch(ArrayIndexOutOfBoundsException e) {
+	                	JourneyMap.getLogger().severe("Bad numbers: ");
+	                	System.out.println(src.length + ", " + srcPos + ", " + dest.length + ", " + destPos + ", " + length);
+	                	break rowcopy;
 	                }
 	            }
 	            pngw.writeRow(line2, row2); // write to full image
 	        }
+	        
 	        for( int tx = 0; tx < nTilesXcur; tx++ ) {
 	            readers[tx].end(); // close readers
 	        }
