0012dc166a3f40c413d8263290f174d53dd3c134
mwoodman <mark.a.woodman@gmail.com>
2013-09-26 20:26:40 -0600

    JourneyMap2.7.3_MC1.6.4

--- src/minecraft/net/minecraft/src/mod_JourneyMap.java
+++ src/minecraft/net/minecraft/src/mod_JourneyMap.java
@@ -12,6 +12,8 @@ import net.techbrew.mcjm.JourneyMap;
 
 public class mod_JourneyMap extends BaseMod {
 	
+	boolean loadSuccess = false;
+	
 	public mod_JourneyMap() {		
 		super();
 	}
@@ -30,21 +32,30 @@ public class mod_JourneyMap extends BaseMod {
 	@Override
 	public void load() 
 	{				
-		JourneyMap instance = JourneyMap.getInstance();
-		instance.initialize(Minecraft.getMinecraft());
-		
-		// Register hooks
-		ModLoader.setInGameHook(this, true, false);
-		ModLoader.setInGUIHook(this, true, false);
-		
-		// Register Map GUI keybinding
-		if(instance.enableMapGui) {
-			ModLoader.registerKey(this, instance.keybinding, false);
+		String version = "?";
+		try {
+			version = JourneyMap.JM_VERSION + " for Minecraft " + JourneyMap.MC_VERSION;
+			JourneyMap instance = JourneyMap.getInstance();
+			instance.initialize(Minecraft.getMinecraft());
+			
+			// Register hooks
+			ModLoader.setInGameHook(this, true, false);
+			ModLoader.setInGUIHook(this, true, false);
+			
+			// Register Map GUI keybinding
+			if(instance.enableMapGui) {
+				ModLoader.registerKey(this, instance.keybinding, false);
+			}
+			
+			loadSuccess = true;
+		} catch(Throwable t) {
+			throw new RuntimeException("Could not load JourneyMap " + version + ". Ensure compatible Minecraft/Modloader/Forge versions.", t);
 		}
 	}
 	
 	@Override
 	public void modsLoaded() {
+		if(!loadSuccess) return;
 		StringBuffer sb = new StringBuffer("ModLoader mods loaded: ");
 		Iterator<BaseMod> iter = new ArrayList<BaseMod>(ModLoader.getLoadedMods()).iterator();
 		while(iter.hasNext()) {
@@ -54,7 +65,7 @@ public class mod_JourneyMap extends BaseMod {
 			}
 		}
 		
-		JourneyMap.getLogger().info(sb.toString());
+		JourneyMap.getLogger().info(sb.toString());		
 	}
 	
 	@Override
@@ -64,11 +75,13 @@ public class mod_JourneyMap extends BaseMod {
 
 	@Override
     public void clientDisconnect(NetClientHandler clientHandler) {
+		if(!loadSuccess) return;
 		JourneyMap.getInstance().stopMapping();
 	}
 	
 	@Override
 	public boolean onTickInGUI(float tick, Minecraft game, GuiScreen gui) {
+		if(!loadSuccess) return false;
 //		if(!(game.entityRenderer instanceof EntityRendererProxy)) {
 //			JourneyMap.getLogger().warning("ModLoader didn't set EntityRendererProxy.  Doing so manually.");
 //			game.entityRenderer = new EntityRendererProxy(game);
@@ -78,6 +91,7 @@ public class mod_JourneyMap extends BaseMod {
 	
 	@Override
 	public boolean onTickInGame(float f, final Minecraft minecraft) {
+		if(!loadSuccess) return false;
 		return JourneyMap.getInstance().onTickInGame(f, minecraft);
 	}
 	
