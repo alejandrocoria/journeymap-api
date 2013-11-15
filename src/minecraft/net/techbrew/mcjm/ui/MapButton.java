e49c313671e6aa5c9c2f2c2e040c06355ffa25bc
mwoodman <mark.a.woodman@gmail.com>
2013-11-15 08:35:42 -0700

    WIP.  This really needs to be overhauled.

--- src/minecraft/net/techbrew/mcjm/ui/MapButton.java
+++ src/minecraft/net/techbrew/mcjm/ui/MapButton.java
@@ -1,9 +1,9 @@
 package net.techbrew.mcjm.ui;
 
-import net.minecraft.src.Minecraft;
-import net.minecraft.src.GuiSmallButton;
-import net.minecraft.src.Tessellator;
 import net.minecraft.src.DynamicTexture;
+import net.minecraft.src.GuiSmallButton;
+import net.minecraft.src.Minecraft;
+import net.minecraft.src.Tessellator;
 import net.techbrew.mcjm.render.overlay.BaseOverlayRenderer;
 
 import org.lwjgl.opengl.GL11;
@@ -47,7 +47,7 @@ public class MapButton extends GuiSmallButton {
 	}
 	
 	public void setHoverText(String hoverText) {
-		multiline = hoverText.split(" "); //$NON-NLS-1$
+		multiline = new String[]{hoverText}; //$NON-NLS-1$
 	}
 	
 	private void updateLabel() {
@@ -73,53 +73,42 @@ public class MapButton extends GuiSmallButton {
             return;
         }
 		
+
 		if(this.icon==null) {
 			super.drawButton(minecraft, mouseX, mouseY);
-			return;
-		}
-		
-		Tessellator tessellator = Tessellator.instance;
-		GL11.glDisable(2929 /*GL_DEPTH_TEST*/);
-		GL11.glDepthMask(false);
-		GL11.glBlendFunc(770, 771);
-		if(enabled) {
-			GL11.glColor4f(1F, 1F, 1F, 1F);
 		} else {
-			GL11.glColor4f(.5F, .5F, .5F, 1F);
+		
+			Tessellator tessellator = Tessellator.instance;
+			GL11.glDisable(2929 /*GL_DEPTH_TEST*/);
+			GL11.glDepthMask(false);
+			GL11.glBlendFunc(770, 771);
+			if(enabled) {
+				GL11.glColor4f(1F, 1F, 1F, 1F);
+			} else {
+				GL11.glColor4f(.5F, .5F, .5F, 1F);
+			}
+			GL11.glDisable(3008 /*GL_ALPHA_TEST*/);		
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, iconTexture.getGlTextureId());
+			
+			// Preserve aspect ratio of source image		
+			int w = getToggled() ? this.width : (int) Math.ceil(this.width*.6);
+			int h = getToggled() ? this.height :  (int) Math.ceil(this.height*.6);
+			int widthOffset = (this.width-w)/2;
+			int heightOffset = (this.height-h);
+	
+			tessellator.startDrawingQuads();
+			tessellator.addVertexWithUV(xPosition + widthOffset, h + yPosition + heightOffset, 0.0D, 0, 1);
+			tessellator.addVertexWithUV(xPosition + w + widthOffset, h + yPosition + heightOffset, 0.0D, 1, 1);
+			tessellator.addVertexWithUV(xPosition + w + widthOffset, yPosition + heightOffset, 0.0D, 1, 0);
+			tessellator.addVertexWithUV(xPosition + widthOffset, yPosition + heightOffset, 0.0D, 0, 0);
+			tessellator.draw();
 		}
-		GL11.glDisable(3008 /*GL_ALPHA_TEST*/);		
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, iconTexture.getGlTextureId());
 		
-		// Check hover
-		boolean hover = mouseX >= xPosition && mouseY >= yPosition && mouseX < xPosition + width && mouseY < yPosition + height;
-
-		// Preserve aspect ratio of source image		
-		int w = getToggled() ? this.width : (int) Math.ceil(this.width*.6);
-		int h = getToggled() ? this.height :  (int) Math.ceil(this.height*.6);
-		int widthOffset = (this.width-w)/2;
-		int heightOffset = (this.height-h);
-
-		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(xPosition + widthOffset, h + yPosition + heightOffset, 0.0D, 0, 1);
-		tessellator.addVertexWithUV(xPosition + w + widthOffset, h + yPosition + heightOffset, 0.0D, 1, 1);
-		tessellator.addVertexWithUV(xPosition + w + widthOffset, yPosition + heightOffset, 0.0D, 1, 0);
-		tessellator.addVertexWithUV(xPosition + widthOffset, yPosition + heightOffset, 0.0D, 0, 0);
-		tessellator.draw();
-		
-        if(hover)
-        {
-        	int color = enabled ? 0xffffa0 : 0xcccccc;
-//        	if(multiline!=null && multiline.length==2) {
-//        		drawCenteredString(minecraft.fontRenderer, multiline[0], xPosition + width / 2, yPosition + (height - 16) / 2, color);
-//        		drawCenteredString(minecraft.fontRenderer, multiline[1], xPosition + width / 2, yPosition + (height - 0) / 2, color);
-//        	} else {
-//        		drawCenteredString(minecraft.fontRenderer, displayString, xPosition + width / 2, yPosition + (height - 8) / 2, color);
-//        	}
-        } 
     }
 
 	//@Override
-    public boolean mousePressed(Minecraft minecraft, int i, int j)
+    @Override
+	public boolean mousePressed(Minecraft minecraft, int i, int j)
     {
         return enabled && drawButton && i >= xPosition && j >= yPosition && i < xPosition + width && j < yPosition + height;
     }
