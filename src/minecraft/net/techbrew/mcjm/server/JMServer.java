2aadfb799f27eaaa820aa20b6a2fff53eba58d9a
mwoodman <mark.a.woodman@gmail.com>
2013-11-18 09:37:45 -0700

    Added validatePort to verify webserver_port or find another one that can be used

--- src/minecraft/net/techbrew/mcjm/server/JMServer.java
+++ src/minecraft/net/techbrew/mcjm/server/JMServer.java
@@ -1,27 +1,108 @@
 package net.techbrew.mcjm.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import net.techbrew.mcjm.JourneyMap;
 import net.techbrew.mcjm.io.PropertyManager;
 import net.techbrew.mcjm.thread.JMThreadFactory;
 import se.rupy.http.Daemon;
 
+/**
+ * Wraps Rupy Daemon and provides thread management.  Tests webserver_port
+ * before starting daemon.
+ * 
+ * @author mwoodman
+ *
+ */
 public class JMServer {
 		
-	private final Daemon rupy;
-	private final int port;
+	private final static int MAXPORT = 9990;
+	private final static int MAXFAILS = 5;
 	
-	public JMServer() {
+	private final Logger logger = JourneyMap.getLogger();
+	
+	private Daemon rupy;
+	private int port;
+	private boolean ready = false;
+	
+	public JMServer() {		
+		port = PropertyManager.getInstance().getInteger(PropertyManager.Key.WEBSERVER_PORT);
+		validatePort();
+	}
+	
+	/**
+	 * Verify port can be bound, try to find another one if not.
+	 */
+	private void validatePort() {
+		
+		int hardFails = 0;
+		int testPort = port;
+		final int maxPort = Math.max(MAXPORT, port + 1000);
+		boolean validPort = false;
+		
+		while(!validPort && hardFails<=MAXFAILS && testPort<=maxPort) {
+			ServerSocketChannel server = null;
+			try {
+				server = ServerSocketChannel.open();
+				server.socket().bind(new InetSocketAddress(testPort));								
+				validPort = true;
+			} catch(java.net.BindException e) {
+				logger.warning("Port " + testPort + " already in use");
+				testPort+=10;
+			} catch (Throwable t) {
+				logger.severe("Error when testing port " + testPort + ": " + t);
+				hardFails++;
+			} finally {
+				if(server != null) {
+					try {
+						server.close();
+					} catch (IOException e) {
+					}
+				}
+			}
+		}
+		
+		ready = validPort;
+		
+		if(ready && port!=testPort) {
+			logger.info("Webserver will use port " + testPort + " for this session");
+			port = testPort;
+		}	
+		
+		if(!ready && hardFails>MAXFAILS) {
+			logger.severe("Gave up finding a port for webserver after " + hardFails + " failures to test ports!");
+		}
+		
+		if(!ready && testPort>MAXPORT) {
+			logger.severe("Gave up finding a port for webserver after testing ports " + port + " - " + maxPort + " without finding one open!");
+		}
+						
+	}
+	
+	public boolean isReady() {
+		return ready;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	
+	public void start() throws Exception {		
+		
+		if(!ready) throw new IllegalStateException("Initialization failed");
 		
 		// Init properties for daemon
 		Properties props = new Properties();
 		
 		// Use port from journeymap properties
-		port = PropertyManager.getInstance().getInteger(PropertyManager.Key.WEBSERVER_PORT);
+
 		props.put("port", Integer.toString(port)); //$NON-NLS-1$
 		props.put("delay", Integer.toString(30000)); //$NON-NLS-1$
 		props.put("timeout", Integer.toString(0)); //$NON-NLS-1$
@@ -35,17 +116,8 @@ public class JMServer {
 		if(logLevel.intValue()<=(Level.FINER.intValue())) {
 			props.put("verbose", Boolean.TRUE.toString()); //$NON-NLS-1$
 		} 
-		
-		// Instantiate daemon
-		rupy = new Daemon(props);
-	}
-	
-	public int getPort() {
-		return port;
-	}
-	
-	public void start() throws Exception {		
-		
+				
+		rupy = new Daemon(props);		
 		rupy.add(new DataService());
 		rupy.add(new LogService());
 		rupy.add(new TileService());
@@ -71,16 +143,16 @@ public class JMServer {
 			}
 		}));
 		
-		JourneyMap.getLogger().info("Started webserver on port " + port); //$NON-NLS-1$
-				
+		logger.info("Started webserver on port " + port); //$NON-NLS-1$
+			
 	}
 	
 	public void stop() {
 		try {
 			rupy.stop();
-			JourneyMap.getLogger().info("Stopped webserver without errors"); //$NON-NLS-1$
+			logger.info("Stopped webserver without errors"); //$NON-NLS-1$
 		} catch (Throwable t) {
-			JourneyMap.getLogger().info("Stopped webserver with error: " + t); //$NON-NLS-1$
+			logger.info("Stopped webserver with error: " + t); //$NON-NLS-1$
 		}
 	}		
 
