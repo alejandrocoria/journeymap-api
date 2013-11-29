f069725836f84f96347356bfeab5d8d23e351927
mwoodman <mark.a.woodman@gmail.com>
2013-11-29 16:53:11 -0700

    Cleaned up imports

--- src/minecraft/net/minecraft/src/mod_JourneyMap.java
+++ src/minecraft/net/minecraft/src/mod_JourneyMap.java
@@ -4,10 +4,6 @@ package net.minecraft.src;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import net.minecraft.src.Minecraft;
-import net.minecraft.src.GuiScreen;
-import net.minecraft.src.NetClientHandler;
-import net.minecraft.src.KeyBinding;
 import net.techbrew.mcjm.JourneyMap;
 
 public class mod_JourneyMap extends BaseMod {
