b4e98ee44b7ae0e300d7f3c9cd7e7583aa4808ec
mwoodman <mark.a.woodman@gmail.com>
2013-12-30 17:49:09 -0700

    Migration to Forge development environment

--- journeymap/resources/journeymap-version.js
+++
@@ -1,3 +0,0 @@
-/* This file on BukkitDev is checked by JourneyMap to see if a newer version exists.*/
-var latest_journeymap_version="0.7.2";
-var latest_minecraft_version="1.8.1";
---
+++ project/journeymap-version.js
@@ -0,0 +1,6 @@
+/*  JourneyMap : http://journeymap.techbrew.net/   */
+/*  This file is checked on startup to see if a newer version is available.  */
+// JM=3.0.0
+// MC=1.6.4
+var latest_journeymap_version="3.0.0";
+var latest_minecraft_version="1.6.4";
