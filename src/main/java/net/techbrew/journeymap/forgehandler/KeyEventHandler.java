bc7b7aa2560a10d50c738af8c5397ee4977e179f
Mark Woodman <mark.a.woodman@gmail.com>
2014-08-13 17:55:58 -0600

    Merged [5b91221] Bugfix: Pressing CTRL (or Command) without another key is "remembered" for the next keypress.

--- src/main/java/net/techbrew/journeymap/forgehandler/KeyEventHandler.java
+++ src/main/java/net/techbrew/journeymap/forgehandler/KeyEventHandler.java
@@ -1,3 +1,11 @@
+/*
+ * JourneyMap mod for Minecraft
+ *
+ * Copyright (C) 2011-2014 Mark Woodman.  All Rights Reserved.
+ * This file may not be altered, file-hosted, re-packaged, or distributed in part or in whole
+ * without express written permission by Mark Woodman <mwoodman@techbrew.net>.
+ */
+
 package net.techbrew.journeymap.forgehandler;
 
 import cpw.mods.fml.client.FMLClientHandler;
@@ -41,7 +49,7 @@ public class KeyEventHandler implements EventHandlerManager.EventHandler
         return EnumSet.of(EventHandlerManager.BusType.FMLCommonHandlerBus);
     }
 
-    @SubscribeEvent
+    @SubscribeEvent()
     public void onKeyboardEvent(InputEvent.KeyInputEvent event)
     {
         KeyEventHandler.onKeypress(false);
@@ -52,79 +60,85 @@ public class KeyEventHandler implements EventHandlerManager.EventHandler
         final int i = Keyboard.getEventKey();
         MapOverlayState mapOverlayState = MapOverlay.state();
 
-        if (GuiScreen.isCtrlKeyDown() && JourneyMap.getInstance().miniMapProperties.enableHotkeys.get())
+        try
         {
+            if (JourneyMap.getInstance().miniMapProperties.enableHotkeys.get())
+            {
+                // This seems to prevent the keycode from "staying"
+                GuiScreen.isCtrlKeyDown();
 
-            if (Constants.isPressed(Constants.KB_MAP))
-            {
-                UIManager.getInstance().toggleMinimap();
-                return;
-            }
-            else if (Constants.isPressed(Constants.KB_MAP_ZOOMIN))
-            {
-                mapOverlayState.zoomIn();
-                return;
-            }
-            else if (Constants.isPressed(Constants.KB_MAP_ZOOMOUT))
-            {
-                mapOverlayState.zoomOut();
-                return;
-            }
-            else if (Constants.isPressed(Constants.KB_MAP_DAY))
-            {
-                mapOverlayState.overrideMapType(Constants.MapType.day);
-                return;
-            }
-            else if (Constants.isPressed(Constants.KB_MAP_NIGHT))
-            {
-                mapOverlayState.overrideMapType(Constants.MapType.night);
-                return;
-            }
-            else if (Constants.isPressed(Constants.KB_MINIMAP_POS))
-            {
-                UIManager.getInstance().getMiniMap().nextPosition();
-                return;
-            }
-            else if (Constants.isPressed(Constants.KB_WAYPOINT))
-            {
-                UIManager.getInstance().openWaypointManager(null, null);
-                return;
-            }
-
-        }
-        else
-        {
-            if (!minimapOnly)
-            {
-                if (Constants.KB_MAP.isPressed())
+                if (GuiScreen.isCtrlKeyDown() && Constants.isPressed(Constants.KB_MAP))
                 {
-                    if (Minecraft.getMinecraft().currentScreen == null)
-                    {
-                        UIManager.getInstance().openMap();
-                    }
-                    else
-                    {
-                        if (Minecraft.getMinecraft().currentScreen instanceof MapOverlay)
-                        {
-                            UIManager.getInstance().closeAll();
-                        }
-                    }
+                    UIManager.getInstance().toggleMinimap();
                     return;
                 }
-                else
+                else if (Constants.isPressed(Constants.KB_MAP_ZOOMIN))
                 {
-                    if (Constants.KB_WAYPOINT.isPressed())
+                    mapOverlayState.zoomIn();
+                    return;
+                }
+                else if (Constants.isPressed(Constants.KB_MAP_ZOOMOUT))
+                {
+                    mapOverlayState.zoomOut();
+                    return;
+                }
+                else if (Constants.isPressed(Constants.KB_MAP_DAY))
+                {
+                    mapOverlayState.overrideMapType(Constants.MapType.day);
+                    return;
+                }
+                else if (Constants.isPressed(Constants.KB_MAP_NIGHT))
+                {
+                    mapOverlayState.overrideMapType(Constants.MapType.night);
+                    return;
+                }
+                else if (Constants.isPressed(Constants.KB_MINIMAP_POS))
+                {
+                    UIManager.getInstance().getMiniMap().nextPosition();
+                    return;
+                }
+                else if (GuiScreen.isCtrlKeyDown() && Constants.isPressed(Constants.KB_WAYPOINT))
+                {
+                    UIManager.getInstance().openWaypointManager(null, null);
+                    return;
+                }
+
+                if (!minimapOnly)
+                {
+                    if (Constants.KB_MAP.isPressed())
                     {
-                        if (Minecraft.getMinecraft().currentScreen == null)
+                        if (FMLClientHandler.instance().getClient().currentScreen == null)
                         {
-                            Minecraft mc = FMLClientHandler.instance().getClient();
-                            Waypoint waypoint = Waypoint.of(mc.thePlayer);
-                            UIManager.getInstance().openWaypointEditor(waypoint, true, null);
+                            UIManager.getInstance().openMap();
+                        }
+                        else
+                        {
+                            if (FMLClientHandler.instance().getClient().currentScreen instanceof MapOverlay)
+                            {
+                                UIManager.getInstance().closeAll();
+                            }
                         }
                         return;
                     }
+                    else
+                    {
+                        if (Constants.KB_WAYPOINT.isPressed())
+                        {
+                            if (FMLClientHandler.instance().getClient().currentScreen == null)
+                            {
+                                Minecraft mc = FMLClientHandler.instance().getClient();
+                                Waypoint waypoint = Waypoint.of(mc.thePlayer);
+                                UIManager.getInstance().openWaypointEditor(waypoint, true, null);
+                            }
+                            return;
+                        }
+                    }
                 }
             }
+        }
+        finally
+        {
+
         }
     }
 }
