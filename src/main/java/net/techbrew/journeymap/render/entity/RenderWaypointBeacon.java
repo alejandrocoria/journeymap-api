5b61fbccd085d27efeed5250fe44bac748b6b262
Mark Woodman <mark.a.woodman@gmail.com>
2014-08-25 00:47:58 -0600

    Merged [ffa3a0d] Removed timers

--- src/main/java/net/techbrew/journeymap/render/entity/RenderWaypointBeacon.java
+++ src/main/java/net/techbrew/journeymap/render/entity/RenderWaypointBeacon.java
@@ -1,5 +1,14 @@
+/*
+ * JourneyMap mod for Minecraft
+ *
+ * Copyright (C) 2011-2014 Mark Woodman.  All Rights Reserved.
+ * This file may not be altered, file-hosted, re-packaged, or distributed in part or in whole
+ * without express written permission by Mark Woodman <mwoodman@techbrew.net>.
+ */
+
 package net.techbrew.journeymap.render.entity;
 
+import cpw.mods.fml.client.FMLClientHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.RenderHelper;
@@ -12,7 +21,6 @@ import net.minecraft.util.Vec3;
 import net.techbrew.journeymap.Constants;
 import net.techbrew.journeymap.JourneyMap;
 import net.techbrew.journeymap.log.LogFormatter;
-import net.techbrew.journeymap.log.StatTimer;
 import net.techbrew.journeymap.model.Waypoint;
 import net.techbrew.journeymap.properties.WaypointProperties;
 import net.techbrew.journeymap.render.draw.DrawUtil;
@@ -30,30 +38,27 @@ import java.util.Collection;
 public class RenderWaypointBeacon
 {
     static final ResourceLocation beam = new ResourceLocation("textures/entity/beacon_beam.png");
-    static StatTimer timer = StatTimer.get("WaypointBeacon.doRender", 100);
-    static StatTimer allTimer = StatTimer.get("WaypointBeacon.renderAll", 100);
-    static Minecraft mc = Minecraft.getMinecraft();
+    //    static StatTimer timer = StatTimer.get("WaypointBeacon.doRender", 100);
+//    static StatTimer allTimer = StatTimer.get("WaypointBeacon.renderAll", 100);
+    static Minecraft mc = FMLClientHandler.instance().getClient();
     static RenderManager renderManager = RenderManager.instance;
-    static String distanceLabel = Constants.getString("Waypoint.distance_meters", "%1.0f");
+    static String distanceLabel = Constants.getString("jm.waypoint.distance_meters", "%1.0f");
     static WaypointProperties waypointProperties;
 
     public static void resetStatTimers()
     {
-        timer.reset();
-        allTimer.reset();
+//        timer.reset();
+//        allTimer.reset();
     }
 
     public static void renderAll()
     {
         try
         {
-            if (waypointProperties == null)
-            {
-                waypointProperties = JourneyMap.getInstance().waypointProperties;
-            }
+            waypointProperties = JourneyMap.getWaypointProperties();
 
             Collection<Waypoint> waypoints = WaypointStore.instance().getAll();
-            allTimer.start();
+            //allTimer.start();
             final int playerDim = mc.thePlayer.dimension;
             for (Waypoint wp : waypoints)
             {
@@ -65,27 +70,30 @@ public class RenderWaypointBeacon
                     }
                     catch (Throwable t)
                     {
-                        JourneyMap.getLogger().severe("EntityWaypoint failed to render for " + wp + ": " + LogFormatter.toString(t));
+                        JourneyMap.getLogger().error("EntityWaypoint failed to render for " + wp + ": " + LogFormatter.toString(t));
                     }
                 }
             }
         }
         catch (Throwable t)
         {
-            allTimer.cancel();
-            JourneyMap.getLogger().severe("Error rendering waypoints: " + LogFormatter.toString(t));
+            //allTimer.cancel();
+            JourneyMap.getLogger().error("Error rendering waypoints: " + LogFormatter.toString(t));
         }
         finally
         {
-            allTimer.stop();
+            //allTimer.stop();
         }
     }
 
     static void doRender(Waypoint waypoint)
     {
-        if(renderManager.livingPlayer==null) return;
+        if (renderManager.livingPlayer == null)
+        {
+            return;
+        }
 
-        timer.start();
+        //timer.start();
 
         RenderHelper.enableStandardItemLighting();
 
@@ -98,7 +106,7 @@ public class RenderWaypointBeacon
 
             // Get waypoint coords for dimension
             Vec3 waypointVec = waypoint.getPosition();
-            waypointVec.yCoord+=.118; // puts icon at eye level
+            waypointVec.yCoord += .118; // puts icon at eye level
 
             // Get view distance from waypoint
             final double actualDistance = playerVec.distanceTo(waypointVec);
@@ -133,7 +141,7 @@ public class RenderWaypointBeacon
 
             // Check for auto-hidden labels
             boolean labelHidden = false;
-            if (viewDistance>.5 && waypointProperties.autoHideLabel.get())
+            if (viewDistance > .5 && waypointProperties.autoHideLabel.get())
             {
                 // 3D algorithm
 //                Vec3 playerLookVec = renderManager.livingPlayer.getLook(1.0F).normalize();
@@ -147,21 +155,24 @@ public class RenderWaypointBeacon
                 // 2D algorithm (ignore pitch)
                 int angle = 5;
 
-                double yaw = Math.atan2(renderManager.viewerPosZ-waypointVec.zCoord, renderManager.viewerPosX-waypointVec.xCoord) ;
+                double yaw = Math.atan2(renderManager.viewerPosZ - waypointVec.zCoord, renderManager.viewerPosX - waypointVec.xCoord);
                 double degrees = Math.toDegrees(yaw) + 90;
-                if(degrees<0) degrees = 360+degrees;
-                double playerYaw = renderManager.livingPlayer.getRotationYawHead()%360;
-                if(playerYaw<0)
+                if (degrees < 0)
+                {
+                    degrees = 360 + degrees;
+                }
+                double playerYaw = renderManager.livingPlayer.getRotationYawHead() % 360;
+                if (playerYaw < 0)
                 {
                     playerYaw += 360;
                 }
                 playerYaw = Math.toRadians(playerYaw);
                 double playerDegrees = Math.toDegrees(playerYaw);
 
-                degrees+=angle;
-                playerDegrees+=angle;
+                degrees += angle;
+                playerDegrees += angle;
 
-                labelHidden = Math.abs((degrees+angle)-(playerDegrees+angle))>angle;
+                labelHidden = Math.abs((degrees + angle) - (playerDegrees + angle)) > angle;
 //
 //                label = String.format("degrees %1.2f & playerDegrees %1.2f = %s", degrees, playerDegrees, labelHidden);
 //                labelHidden = false;
@@ -175,14 +186,14 @@ public class RenderWaypointBeacon
             double halfTexHeight = texture.height / 2;
 
             // Depth-masked and non-masked label
-            final boolean showName = waypointProperties.showName.get() && label!=null && label.length()>0;
+            final boolean showName = waypointProperties.showName.get() && label != null && label.length() > 0;
             final boolean showDistance = waypointProperties.showDistance.get();
             if (!labelHidden && (showName || showDistance))
             {
                 // Construct label
                 StringBuilder sb = new StringBuilder();
 
-                if(waypointProperties.boldLabel.get())
+                if (waypointProperties.boldLabel.get())
                 {
                     sb.append(EnumChatFormatting.BOLD);
                 }
@@ -190,7 +201,7 @@ public class RenderWaypointBeacon
                 {
                     sb.append(label);
                 }
-                if(showName && showDistance)
+                if (showName && showDistance)
                 {
                     sb.append(" ");
                 }
@@ -199,7 +210,7 @@ public class RenderWaypointBeacon
                     sb.append(String.format(distanceLabel, actualDistance));
                 }
 
-                if(sb.length()>0)
+                if (sb.length() > 0)
                 {
                     label = sb.toString();
 
@@ -224,7 +235,7 @@ public class RenderWaypointBeacon
                     boolean forced = DrawUtil.startUnicode(fr, waypointProperties.forceUnicode.get());
 
                     // Adjust above icon
-                    double labelY = (0-halfTexHeight)-8;
+                    double labelY = (0 - halfTexHeight) - 8;
 
                     // Depth label
                     DrawUtil.drawLabel(label, 1, labelY, DrawUtil.HAlign.Center, DrawUtil.VAlign.Above, Color.black, 150, waypoint.getSafeColor(), 255, fontScale, false);
@@ -244,7 +255,7 @@ public class RenderWaypointBeacon
             }
 
             // Depth-masked icon
-            if (viewDistance>.1 && waypointProperties.showTexture.get())
+            if (viewDistance > .1 && waypointProperties.showTexture.get())
             {
                 // Reset scale for the icon
 
@@ -265,7 +276,7 @@ public class RenderWaypointBeacon
                 GL11.glNormal3d(0, 0, -1.0F * scale);
 
                 // The .5 and .2 below centers the waypoint diamond icon
-                DrawUtil.drawColoredImage(texture, 255, waypoint.getColor(), 0 - (texture.width / 2) +.5, 0 - halfTexHeight + .2);
+                DrawUtil.drawColoredImage(texture, 255, waypoint.getColor(), 0 - (texture.width / 2) + .5, 0 - halfTexHeight + .2);
 
                 GL11.glPopMatrix();
             }
@@ -276,12 +287,12 @@ public class RenderWaypointBeacon
             GL11.glDepthMask(true);
             GL11.glEnable(GL11.GL_DEPTH_TEST);
             GL11.glEnable(GL11.GL_LIGHTING);
-           // GL11.glEnable(GL11.GL_ALPHA_TEST);
+            // GL11.glEnable(GL11.GL_ALPHA_TEST);
 
             // Reset GL
 
 
-            timer.stop();
+            //timer.stop();
 
             GL11.glDepthMask(true);
             GL11.glEnable(GL11.GL_CULL_FACE);
@@ -310,10 +321,10 @@ public class RenderWaypointBeacon
         GL11.glDepthMask(false);
 
         float time = (float) mc.theWorld.getTotalWorldTime();
-        if(mc.isGamePaused())
+        if (mc.isGamePaused())
         {
             // Show rotation for Waypoint Options UI
-            time = Minecraft.getSystemTime()/50;
+            time = Minecraft.getSystemTime() / 50;
         }
 
         float texOffset = -(-time * 0.2F - (float) MathHelper.floor_float(-time * 0.1F)) * .6f;
@@ -326,7 +337,7 @@ public class RenderWaypointBeacon
             tessellator.setColorRGBA(color.getRed(), color.getGreen(), color.getBlue(), 80);
             double d4 = (double) b0 * 0.2D;
             double d5 = Math.cos(d3 + 2.356194490192345D) * d4;
-            double d6 =  Math.sin(d3 + 2.356194490192345D) * d4;
+            double d6 = Math.sin(d3 + 2.356194490192345D) * d4;
             double d7 = Math.cos(d3 + (Math.PI / 4D)) * d4;
             double d8 = Math.sin(d3 + (Math.PI / 4D)) * d4;
             double d9 = Math.cos(d3 + 3.9269908169872414D) * d4;
@@ -365,8 +376,8 @@ public class RenderWaypointBeacon
             double d29 = (double) (-1.0F + texOffset);
             double d30 = (double) (256.0F * f1) + d29;
 
-            x-=.5;
-            z-=.5;
+            x -= .5;
+            z -= .5;
 
             tessellator.startDrawingQuads();
             tessellator.setColorRGBA(color.getRed(), color.getGreen(), color.getBlue(), 40);
