b3326f305d9a8eff01c9ec847c2c78bd809ce3e2
Mark Woodman <mark.a.woodman@gmail.com>
2014-09-22 14:51:53 -0600

    Belonged with Color Palette commits

--- src/main/java/net/techbrew/journeymap/model/BlockMD.java
+++ src/main/java/net/techbrew/journeymap/model/BlockMD.java
@@ -19,11 +19,14 @@ import java.awt.*;
 import java.io.Serializable;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.Random;
 
 
-public class BlockMD implements Serializable {
+public class BlockMD implements Serializable
+{
 
-	private static final long serialVersionUID = 2L;
+    private static final Random random = new Random();
+    private static final long serialVersionUID = 2L;
     final static HashMap<Block, GameRegistry.UniqueIdentifier> blockUids = new HashMap<Block, GameRegistry.UniqueIdentifier>();
 
     public final static class CacheKey implements Serializable
@@ -31,30 +34,46 @@ public class BlockMD implements Serializable {
         public final GameRegistry.UniqueIdentifier uid;
         public final int meta;
 
-        public CacheKey(GameRegistry.UniqueIdentifier uid, int meta) {
+        public CacheKey(GameRegistry.UniqueIdentifier uid, int meta)
+        {
             this.uid = uid;
             this.meta = meta;
         }
 
         @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof CacheKey)) return false;
+        public boolean equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (!(o instanceof CacheKey))
+            {
+                return false;
+            }
             CacheKey cacheKey = (CacheKey) o;
-            if (meta != cacheKey.meta) return false;
-            if (!uid.equals(cacheKey.uid)) return false;
+            if (meta != cacheKey.meta)
+            {
+                return false;
+            }
+            if (!uid.equals(cacheKey.uid))
+            {
+                return false;
+            }
             return true;
         }
 
         @Override
-        public int hashCode() {
+        public int hashCode()
+        {
             int result = uid.hashCode();
             result = 31 * result + meta;
             return result;
         }
 
         @Override
-        public String toString() {
+        public String toString()
+        {
             return uid + ":" + meta;
         }
     }
@@ -62,43 +81,54 @@ public class BlockMD implements Serializable {
     private static final LoadingCache<CacheKey, BlockMD> cache = CacheBuilder.newBuilder()
             .concurrencyLevel(1)
             .initialCapacity(256)
-            .build(new CacheLoader<CacheKey, BlockMD>() {
+            .build(new CacheLoader<CacheKey, BlockMD>()
+            {
                 @Override
-                public BlockMD load(CacheKey key) throws Exception {
-                try {
-                    return createBlockMD(key);
-                } catch (Exception e) {
-                    throw e;
-                }
+                public BlockMD load(CacheKey key) throws Exception
+                {
+                    try
+                    {
+                        return createBlockMD(key);
+                    }
+                    catch (Exception e)
+                    {
+                        throw e;
+                    }
                 }
             });
 
     public final CacheKey key;
     private transient Block block;
-	private Color color;
-	private float alpha;
+    private Color color;
+    private float alpha;
     private AlphaComposite alphaComposite;
     private final EnumSet<BlockUtils.Flag> flags;
     private final String name;
 
     /**
      * Produces a BlockMD instance.
+     *
      * @param chunkMd
      * @param x
      * @param y
      * @param z
      * @return
      */
-    public static BlockMD getBlockMD(ChunkMD chunkMd, int x, int y, int z) {
-        try {
+    public static BlockMD getBlockMD(ChunkMD chunkMd, int x, int y, int z)
+    {
+        try
+        {
             Block block;
             int meta;
             boolean isAir = false;
-            if(y>=0) {
+            if (y >= 0)
+            {
                 block = chunkMd.stub.getBlock(x, y, z);
                 isAir = block.isAir(chunkMd.worldObj, x, y, z);
                 meta = (isAir) ? 0 : chunkMd.stub.getBlockMetadata(x, y, z);
-            } else {
+            }
+            else
+            {
                 block = Blocks.bedrock;
                 meta = 0;
             }
@@ -106,16 +136,22 @@ public class BlockMD implements Serializable {
             CacheKey key = new CacheKey(findUniqueIdentifierFor(block), meta);
             return cache.get(key);
 
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             JourneyMap.getLogger().severe("Can't get blockId/meta for chunk " + chunkMd.stub.xPosition + "," + chunkMd.stub.zPosition + " block " + x + "," + y + "," + z + ": " + LogFormatter.toString(e)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
             return null;
         }
     }
 
-    public static BlockMD getBlockMD(GameRegistry.UniqueIdentifier uid, int meta) {
-        try {
+    public static BlockMD getBlockMD(GameRegistry.UniqueIdentifier uid, int meta)
+    {
+        try
+        {
             return cache.get(new CacheKey(uid, meta));
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             JourneyMap.getLogger().severe("Can't get BlockMD for block " + uid + " meta " + meta + ": " + LogFormatter.toString(e));
             return null;
         }
@@ -126,15 +162,46 @@ public class BlockMD implements Serializable {
         return blockUids.get(block);
     }
 
-    private static final BlockMD createBlockMD(CacheKey key) {
+    public static String getBlockName(Block block, int meta)
+    {
+        String name = block.getUnlocalizedName();
+        try
+        {
+            // Gotta love this.
+            Item item = Item.getItemFromBlock(block);
+            if (item == null)
+            {
+                item = block.getItemDropped(0, random, 0);
+            }
+            if (item != null)
+            {
+                ItemStack stack = new ItemStack(item, 1, block.damageDropped(meta));
+                name = stack.getDisplayName();
+            }
+        }
+        catch (Throwable t)
+        {
+            JourneyMap.getLogger().fine("Displayname not available for " + name);
+        }
+
+        if (name.startsWith("tile"))
+        {
+            name = block.getClass().getSimpleName().replaceAll("Block", "");
+        }
+        return name;
+    }
+
+    private static final BlockMD createBlockMD(CacheKey key)
+    {
 
         Block block = GameRegistry.findBlock(key.uid.modId, key.uid.name);
-        if(block==null)
+        if (block == null)
         {
             block = GameRegistry.findBlock(null, key.uid.name);
-            if(block==null)
+            if (block == null)
             {
-                if(!key.uid.name.equals("Air")) {
+                if (!key.uid.name.equals("Air"))
+                {
                     JourneyMap.getLogger().warning("Block not found for " + key.uid);
                     return new BlockMD(key, Blocks.air, "ERROR-" + block.getClass().getName());
                 }
@@ -145,33 +212,21 @@ public class BlockMD implements Serializable {
             }
         }
 
-        String prefix = "";
-        String suffix = ":" + key.meta;
-        String name = key.uid.toString() + suffix;
-        try {
-            // Gotta love this.
-            Item item = Item.getItemFromBlock(block);
-            ItemStack stack = new ItemStack(item, 1, block.damageDropped(key.meta));
-            String displayName = stack.getDisplayName();
-
-            if(!key.uid.modId.equals("minecraft")){
-                prefix = key.uid.modId+":";
-            }
-
-            name = prefix + displayName + suffix;
-
-        } catch(Throwable t) {
-            JourneyMap.getLogger().fine("Displayname not available for " + name);
-        }
-
+        String name = getBlockName(block, key.meta);
         BlockMD blockMD = new BlockMD(key, block, name);
-        if(blockMD.isAir()) {
+        if (blockMD.isAir())
+        {
             blockMD.color = Color.CYAN; // Should be obvious if it gets displayed somehow.
             blockMD.setAlpha(0f);
-        } else {
-            if(BlockUtils.hasAlpha(block)) {
+        }
+        else
+        {
+            if (BlockUtils.hasAlpha(block))
+            {
                 blockMD.setAlpha(BlockUtils.getAlpha(block));
-            } else {
+            }
+            else
+            {
                 blockMD.setAlpha(1F);
             }
         }
@@ -181,13 +236,17 @@ public class BlockMD implements Serializable {
         return blockMD;
     }
 
-	private BlockMD(CacheKey key, Block block, String name) {
-        if(block==null) throw new IllegalArgumentException("Block can't be null");
+    private BlockMD(CacheKey key, Block block, String name)
+    {
+        if (block == null)
+        {
+            throw new IllegalArgumentException("Block can't be null");
+        }
         this.key = key;
-		this.block = block;
+        this.block = block;
         this.name = name;
         this.flags = BlockUtils.getFlags(this.key.uid);
-	}
+    }
 
     public boolean hasFlag(BlockUtils.Flag flag)
     {
@@ -196,139 +255,178 @@ public class BlockMD implements Serializable {
 
     public void addFlags(BlockUtils.Flag... addFlags)
     {
-        for(BlockUtils.Flag flag : addFlags) {
+        for (BlockUtils.Flag flag : addFlags)
+        {
             this.flags.add(flag);
         }
     }
-	
-	public RGB getColor(ChunkMD chunkMd, int x, int y, int z) {
 
-        if(isAir())
+    public RGB getColor(ChunkMD chunkMd, int x, int y, int z)
+    {
+
+        if (isAir())
         {
             // This shouldn't be called
             JourneyMap.getLogger();
         }
 
-		if(this.color!=null) {
+        if (this.color != null)
+        {
             return new RGB(this.color);
-        } else {
+        }
+        else
+        {
 
-			Color color = ColorCache.getInstance().getBlockColor(chunkMd, this, x, y, z);
-            if(color==null) {
+            Color color = ColorCache.getInstance().getBlockColor(chunkMd, this, x, y, z);
+            if (color == null)
+            {
                 return new RGB(Color.BLACK);
             }
 
-            if(isBiomeColored()) {
+            if (isBiomeColored())
+            {
                 return new RGB(color);
-            } else {
+            }
+            else
+            {
                 this.color = color;
                 return new RGB(color);
             }
-		}
-	}
+        }
+    }
 
-	public void setAlpha(float alpha) {
-		this.alpha = alpha;
-        if(alpha<1f) {
+    public void setAlpha(float alpha)
+    {
+        this.alpha = alpha;
+        if (alpha < 1f)
+        {
             this.flags.add(BlockUtils.Flag.Transparency);
             alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha);
-        } else {
-            if(this.hasFlag(BlockUtils.Flag.Transparency))
+        }
+        else
+        {
+            if (this.hasFlag(BlockUtils.Flag.Transparency))
             {
                 this.flags.remove(BlockUtils.Flag.Transparency);
             }
             alphaComposite = null;
         }
-	}
+    }
 
-    public float getAlpha() {
+    public float getAlpha()
+    {
         return alpha;
     }
 
-    public AlphaComposite getAlphaComposite() {
-        return (alphaComposite==null) ? BlockUtils.OPAQUE : alphaComposite;
+    public AlphaComposite getAlphaComposite()
+    {
+        return (alphaComposite == null) ? BlockUtils.OPAQUE : alphaComposite;
     }
-	
-	public Block getBlock() {
-        if(block==null){
+
+    public Block getBlock()
+    {
+        if (block == null)
+        {
             block = GameRegistry.findBlock(key.uid.modId, key.uid.name);
-            if(block==null){
+            if (block == null)
+            {
                 block = Blocks.air;
             }
         }
         return block;
-	}
+    }
 
-    public boolean isTransparent() {
+    public boolean isTransparent()
+    {
         return block.getMaterial() == Material.air;
     }
 
-    public boolean isAir() {
+    public boolean isAir()
+    {
         return hasFlag(BlockUtils.Flag.HasAir);
     }
 
-    public boolean isIce() {
-        return block==Blocks.ice;
+    public boolean isIce()
+    {
+        return block == Blocks.ice;
     }
 
-    public boolean isTorch() {
+    public boolean isTorch()
+    {
         getBlock();
-        return block== Blocks.torch||block==Blocks.redstone_torch||block==Blocks.unlit_redstone_torch;
+        return block == Blocks.torch || block == Blocks.redstone_torch || block == Blocks.unlit_redstone_torch;
     }
 
-    public boolean isWater() {
+    public boolean isWater()
+    {
         getBlock();
-        return block== Blocks.water||block==Blocks.flowing_water;
+        return block == Blocks.water || block == Blocks.flowing_water;
     }
 
-    public boolean isTransparentRoof() {
+    public boolean isTransparentRoof()
+    {
         return hasFlag(BlockUtils.Flag.TransparentRoof);
     }
 
-    public boolean isLava() {
+    public boolean isLava()
+    {
         getBlock();
-        return block== Blocks.lava||block==Blocks.flowing_lava;
+        return block == Blocks.lava || block == Blocks.flowing_lava;
     }
 
-    public boolean isFoliage() {
+    public boolean isFoliage()
+    {
         return getBlock() instanceof BlockLeaves;
     }
 
-    public boolean isBiomeColored() {
+    public boolean isBiomeColored()
+    {
         return flags.contains(BlockUtils.Flag.BiomeColor) || flags.contains(BlockUtils.Flag.CustomBiomeColor);
     }
 
-	@Override
-	public int hashCode() {
-		return key.hashCode();
-	}
+    @Override
+    public int hashCode()
+    {
+        return key.hashCode();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof BlockMD))
-			return false;
-		BlockMD other = (BlockMD) obj;
-		return key.equals(other.key);
-	}
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (!(obj instanceof BlockMD))
+        {
+            return false;
+        }
+        BlockMD other = (BlockMD) obj;
+        return key.equals(other.key);
+    }
 
-	@Override
-	public String toString() {
-		return "BlockMD [" + key.uid + " meta " + key.meta + "]";
-	}
+    @Override
+    public String toString()
+    {
+        return "BlockMD [" + key.uid + " meta " + key.meta + "]";
+    }
 
     /**
      * Use with care, since this creates an itemstack just to get the name.
+     *
      * @return
      */
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public static void clearCache() {
+    public static void clearCache()
+    {
         cache.invalidateAll();
     }
 
