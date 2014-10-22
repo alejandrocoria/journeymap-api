139a2b4046edb0de550c7230022303922e53e8db
techbrew <mark.a.woodman@gmail.com>
2014-10-22 10:42:31 -0600

    Bugfix:  Waypoints weren't consistently loaded after cache reset by dimension switches, deaths, etc.

--- src/main/java/net/techbrew/journeymap/waypoint/WaypointStore.java
+++ src/main/java/net/techbrew/journeymap/waypoint/WaypointStore.java
@@ -123,30 +123,34 @@ public class WaypointStore
         cache.invalidateAll();
         dimensions.clear();
         loaded = false;
+        if (JourneyMap.getWaypointProperties().managerEnabled.get())
+        {
+            load();
+        }
     }
 
-    public void load()
+    private void load()
     {
         synchronized (cache)
         {
-            cache.invalidateAll();
-
+            ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+            File waypointDir = null;
             try
             {
-                File waypointDir = FileHandler.getWaypointDir();
+                cache.invalidateAll();
 
-                ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+                waypointDir = FileHandler.getWaypointDir();
                 waypoints.addAll(new ReiReader().loadWaypoints(waypointDir, true));
                 waypoints.addAll(new VoxelReader().loadWaypoints(waypointDir, true));
                 waypoints.addAll(new JmReader().loadWaypoints(waypointDir));
 
                 load(waypoints, false);
 
-                JourneyMap.getLogger().info(String.format("Loaded %s waypoints for world", cache.size()));
+                JourneyMap.getLogger().info(String.format("Loaded %s waypoints from %s", cache.size(), waypointDir));
             }
             catch (Exception e)
             {
-                JourneyMap.getLogger().error(String.format("Error loading waypoints: %s", LogFormatter.toString(e)));
+                JourneyMap.getLogger().error(String.format("Error loading waypoints from %s: %s", waypointDir, LogFormatter.toString(e)));
             }
         }
     }
