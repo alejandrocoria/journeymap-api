e864c5df2f4344ca05903081239dfaf23bfa38ec
techbrew <mark.a.woodman@gmail.com>
2014-12-23 12:49:55 -0700

    removed checks for whether image actually changed after chunk insert.  Too expensive.

--- src/main/java/net/techbrew/journeymap/model/RegionImageSet.java
+++ src/main/java/net/techbrew/journeymap/model/RegionImageSet.java
@@ -12,18 +12,11 @@ import net.techbrew.journeymap.Constants;
 import net.techbrew.journeymap.Constants.MapType;
 import net.techbrew.journeymap.JourneyMap;
 import net.techbrew.journeymap.io.RegionImageHandler;
-import net.techbrew.journeymap.log.StatTimer;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.DataBufferByte;
-import java.awt.image.DataBufferInt;
 import java.io.File;
-import java.util.Arrays;
-import java.util.concurrent.TimeUnit;
 
 /**
  * A RegionImageSet contains one or more Wrappers of image, file, and maptype.
@@ -45,10 +38,8 @@ public class RegionImageSet extends ImageSet
     @Override
     protected Wrapper getWrapper(Constants.MapType mapType)
     {
-
         synchronized (lock)
         {
-
             // Check wrappers
             Wrapper wrapper = imageWrappers.get(mapType);
             if (wrapper != null)
@@ -99,135 +90,27 @@ public class RegionImageSet extends ImageSet
         }
     }
 
-    public void insertChunk(final ChunkImageSet cis, final boolean forceFlush)
+    public void insertChunk(final ChunkImageSet cis)
     {
         synchronized (lock)
         {
             for (ChunkImageSet.Wrapper cisWrapper : cis.imageWrappers.values())
             {
-                insertChunk(cis.getCCoord(), cisWrapper.getImage(), cisWrapper.getMapType(), forceFlush);
+                insertChunk(cis.getCCoord(), cisWrapper.getImage(), cisWrapper.getMapType());
             }
         }
     }
 
-    protected void insertChunk(ChunkCoord cCoord, BufferedImage chunkImage, MapType mapType, final boolean forceFlush)
+    protected void insertChunk(ChunkCoord cCoord, BufferedImage chunkImage, MapType mapType)
     {
         final Wrapper wrapper = getWrapper(mapType);
-        final boolean wasDirty = wrapper.isDirty();
         final int x = rCoord.getXOffset(cCoord.chunkX);
         final int z = rCoord.getZOffset(cCoord.chunkZ);
         final BufferedImage subRegion = wrapper.getImage().getSubimage(x, z, 16, 16);
-        final DataBuffer before = subRegion.getData().getDataBuffer();
         final Graphics2D g2d = subRegion.createGraphics();
         g2d.drawImage(chunkImage, 0, 0, null);
         g2d.dispose();
-        final DataBuffer after = subRegion.getData().getDataBuffer();
-
-        //logger.info("Insert da chunk! " + cCoord);
-
-        // check the buffers to see if anything changed
-        long start = System.nanoTime();
-        boolean dirty = forceFlush;
-
-        if (!dirty)
-        {
-            dirty = before.getDataType() != after.getDataType() || before.getSize() != after.getSize() || before.getNumBanks() != after.getNumBanks();
-        }
-
-        if (!dirty)
-        {
-            if (before.getClass() != after.getClass())
-            {
-                dirty = true;
-                if (logger.isEnabled(Level.DEBUG))
-                {
-                    logger.debug("Classes don't match: " + before.getClass() + " vs " + after.getClass());
-                }
-            }
-            else
-            {
-                if (before instanceof DataBufferByte)
-                {
-                    dirty = bufferChanged((DataBufferByte) before, (DataBufferByte) after);
-                }
-                else if (before instanceof DataBufferInt)
-                {
-                    dirty = bufferChanged((DataBufferInt) before, (DataBufferInt) after);
-                }
-                else
-                {
-                    dirty = true;
-                }
-            }
-        }
-
-        if (dirty)
-        {
-            if (!wasDirty)
-            {
-                if (logger.isEnabled(Level.DEBUG))
-                {
-                    long stop = System.nanoTime();
-                    logger.debug(rCoord + " dirty after chunk insert " + cCoord + ": " + dirty + ", compared in: " + TimeUnit.NANOSECONDS.toMicros(stop - start) + "micros");
-                }
-            }
-            wrapper.setDirty(); // updates the timestamp
-        }
-    }
-
-    private boolean bufferChanged(DataBufferInt before, DataBufferInt after)
-    {
-        StatTimer timer = StatTimer.get("RegionImageSet.bufferChanged").start();
-        boolean changed = false;
-        for (int bank = 0; bank < after.getNumBanks(); bank++)
-        {
-            int[] afterBank = after.getData(bank);
-            int[] beforeBank = before.getData(bank);
-
-            // this line may vary depending on your test framework
-            changed = !Arrays.equals(afterBank, beforeBank);
-            if (changed)
-            {
-                break;
-            }
-        }
-        timer.stop();
-        return changed;
-    }
-
-    private boolean bufferChanged(DataBufferByte before, DataBufferByte after)
-    {
-        StatTimer timer = StatTimer.get("RegionImageSet.bufferChanged").start();
-        boolean changed = false;
-        for (int bank = 0; bank < after.getNumBanks(); bank++)
-        {
-            byte[] afterBank = after.getData(bank);
-            byte[] beforeBank = before.getData(bank);
-
-            // this line may vary depending on your test framework
-            changed = !Arrays.equals(afterBank, beforeBank);
-            if (changed)
-            {
-                break;
-            }
-        }
-        timer.stop();
-        return changed;
-    }
-
-    public void loadLegacyNormal(File legacyFile)
-    {
-        if (legacyFile.exists())
-        {
-            BufferedImage legacyImage = RegionImageHandler.getImage(legacyFile);
-            addWrapperFromLegacy(Constants.MapType.day, legacyImage);
-            addWrapperFromLegacy(Constants.MapType.night, legacyImage);
-            addWrapper(Constants.MapType.OBSOLETE, legacyFile, null);
-        }
-        else
-        {
-            throw new IllegalStateException("legacy file doesn't exist: " + legacyFile);
-        }
+        wrapper.setDirty();
     }
 
     @Override
