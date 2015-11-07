2949ed756d80c33719fc291593c1981f1b189777
techbrew <mark.a.woodman@gmail.com>
2015-11-06 17:20:54 -0700

    Added mechanism to call methods on JmUI classes; support to apply new Vault theme from Splash dialog

--- src/main/java/journeymap/client/model/SplashInfo.java
+++ src/main/java/journeymap/client/model/SplashInfo.java
@@ -15,6 +15,7 @@ import journeymap.client.ui.component.JmUI;
 import journeymap.client.ui.dialog.OptionsManager;
 import journeymap.common.Journeymap;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 /**
@@ -50,21 +51,49 @@ public class SplashInfo
             }
             String[] parts = this.action.split("#");
             String className = parts[0];
+            String action = null;
+            if(parts.length>0)
+            {
+                action = parts[1];
+            }
 
             try
             {
-                Class<? extends JmUI> uiClass = (Class<? extends JmUI>) Class.forName("journeymap.client.ui.dialog." + className);
+                Class<? extends JmUI> uiClass = (Class<? extends JmUI>) Class.forName("journeymap.client.ui." + className);
 
-                if (uiClass.equals(OptionsManager.class) && parts.length > 0)
+                if (uiClass.equals(OptionsManager.class) && action!=null)
                 {
-                    Config.Category category = Config.Category.valueOf(parts[1]);
+                    Config.Category category = Config.Category.valueOf(action);
                     UIManager.getInstance().openOptionsManager(returnUi, category);
+                    return;
                 }
-                else
+                else if(action!=null)
                 {
-                    UIManager.getInstance().open(uiClass, returnUi);
+                    String arg = parts.length==3 ? parts[2] : null;
+                    Method actionMethod;
+                    try
+                    {
+                        JmUI ui = UIManager.getInstance().open(uiClass, returnUi);
+
+                        if(arg==null)
+                        {
+                            actionMethod = uiClass.getMethod(action);
+                            actionMethod.invoke(ui);
+                        }
+                        else
+                        {
+                            actionMethod = uiClass.getMethod(action, String.class);
+                            actionMethod.invoke(ui, arg);
+                        }
+                        return;
+                    }
+                    catch(Exception e)
+                    {
+                        Journeymap.getLogger().warn("Couldn't perform action " + action + " on " + uiClass + ": " + e.getMessage());
+                    }
                 }
 
+                UIManager.getInstance().open(uiClass, returnUi);
             }
             catch (Throwable t)
             {
