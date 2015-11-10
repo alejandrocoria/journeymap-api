54557d745e267a170eb8a2b20f240eda6163fcb4
mysticdrew <drewhaas@gmail.com>
2015-11-10 12:36:48 -0600

    Extra checks for disabled worldId [27f3bd6]

--- src/main/java/journeymap/common/network/WorldIDPacket.java
+++ src/main/java/journeymap/common/network/WorldIDPacket.java
@@ -16,6 +16,7 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.relauncher.Side;
 import io.netty.buffer.ByteBuf;
 import journeymap.common.Journeymap;
+import journeymap.server.oldservercode.config.ConfigHandler;
 import net.minecraft.entity.player.EntityPlayerMP;
 // 1.8
 //import net.minecraftforge.fml.common.network.ByteBufUtils;
@@ -82,12 +83,14 @@ public class WorldIDPacket implements IMessage
         @Override
         public IMessage onMessage(WorldIDPacket message, MessageContext ctx)
         {
+
             EntityPlayerMP player = null;
             if (ctx.side == Side.SERVER) {
-               player =  ctx.getServerHandler().playerEntity;
+                player =  ctx.getServerHandler().playerEntity;
+                if (ConfigHandler.getConfigByWorldName(player.getEntityWorld().getWorldInfo().getWorldName()).isUsingWorldID()) {
+                    Journeymap.proxy.handleWorldIdMessage(message.getWorldID(), player);
+                }
             }
-
-            Journeymap.proxy.handleWorldIdMessage(message.getWorldID(), player);
             return null;
         }
     }
