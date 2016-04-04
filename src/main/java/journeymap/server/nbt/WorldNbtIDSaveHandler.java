9d4b8fe86e2f14c197fb763f229530959ca84c29
mysticdrew <drewhaas@gmail.com>
2016-04-03 22:23:17 -0500

    Fix NBT Migration NPE

--- src/main/java/journeymap/server/nbt/WorldNbtIDSaveHandler.java
+++ src/main/java/journeymap/server/nbt/WorldNbtIDSaveHandler.java
@@ -49,7 +49,7 @@ public class WorldNbtIDSaveHandler
 
         // TODO: Remove this migration when we update to MC 1.9+
         // Migrate old worldID to new system.
-        if (legacyData.getData().hasKey(LEGACY_WORLD_ID_KEY))
+        if (legacyData != null && legacyData.getData().hasKey(LEGACY_WORLD_ID_KEY))
         {
             String worldId = legacyData.getData().getString(LEGACY_WORLD_ID_KEY);
 
