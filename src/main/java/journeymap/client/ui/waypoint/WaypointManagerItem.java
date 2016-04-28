18475cb922eb03823bac6fc37fb5411a82e30af1
techbrew <mark.a.woodman@gmail.com>
2016-04-28 15:25:13 -0600

    Bugfix: (journeymap #56) Waypoint Manager's sort-by-name should ignore case

--- src/main/java/journeymap/client/ui/waypoint/WaypointManagerItem.java
+++ src/main/java/journeymap/client/ui/waypoint/WaypointManagerItem.java
@@ -448,11 +448,11 @@ public class WaypointManagerItem implements ScrollListPane.ISlot
         {
             if (ascending)
             {
-                return o1.waypoint.getName().compareTo(o2.waypoint.getName());
+                return o1.waypoint.getName().compareToIgnoreCase(o2.waypoint.getName());
             }
             else
             {
-                return o2.waypoint.getName().compareTo(o1.waypoint.getName());
+                return o2.waypoint.getName().compareToIgnoreCase(o1.waypoint.getName());
             }
         }
     }
