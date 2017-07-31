760035aa42fa0b1052dd325c1662b9b55f273f8f
mysticdrew <drewhaas@gmail.com>
2017-07-30 21:56:42 -0500

    Re-fixed op teleport issue

--- src/main/java/journeymap/server/events/ForgeEvents.java
+++ src/main/java/journeymap/server/events/ForgeEvents.java
@@ -80,43 +80,26 @@ public class ForgeEvents
     {
         if (event.player instanceof EntityPlayerMP)
         {
-            EntityPlayerMP player = (EntityPlayerMP) event.player;
-            Boolean hasForge = player.connection.getNetworkManager().channel().attr(NetworkRegistry.FML_MARKER).get();
-            if (!hasForge) {
-                Journeymap.getLogger().info(player.getName()+ " is connecting with a vanilla client, ignoring journeymap login permissions.");
-                return;
-            }
             if (PropertiesManager.getInstance().getGlobalProperties().useWorldId.get())
             {
                 PacketHandler.sendPlayerWorldID((EntityPlayerMP) event.player);
             }
 
+            InitLogin init = new InitLogin();
+
             if (PropertiesManager.getInstance().getGlobalProperties().teleportEnabled.get())
             {
-                InitLogin init = new InitLogin();
-                boolean canPlayerTeleport;
-                if (JourneyMapTeleport.isOp(player))
-                {
-                    canPlayerTeleport = true;
-                }
-                else
-                {
-                    canPlayerTeleport = PropertiesManager.getInstance().getGlobalProperties().teleportEnabled.get();
-                }
-                init.setTeleportEnabled(canPlayerTeleport);
-                PacketHandler.sendLoginPacket((EntityPlayerMP) event.player, init);
+                init.setTeleportEnabled(true);
             }
-
-            if (isOp((EntityPlayerMP) event.player))
+            else if (isOp((EntityPlayerMP) event.player))
             {
-                //TODO: send op ui packet
-//                System.out.println("PLAYER IS OPS");
+                init.setTeleportEnabled(true);
             }
             else
             {
-                //TODO: sendplayer logged in packet(worldid)
-//                System.out.println("NOT OP");
+                init.setTeleportEnabled(false);
             }
+            PacketHandler.sendLoginPacket((EntityPlayerMP) event.player, init);
         }
     }
 
